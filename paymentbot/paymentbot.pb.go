0e00d407f3997868c22ff3fe3873be65cadb3256
IgorKornelli <igor.kornelli@crypterium.com>
2021-02-22 22:02:24 +0300

    feat: add messagesForAnswer

--- paymentbot/paymentbot.pb.go
+++ paymentbot/paymentbot.pb.go
@@ -27,11 +27,12 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type Request struct {
-	Subject              string   `protobuf:"bytes,1,opt,name=subject,proto3" json:"subject,omitempty"`
-	Message              string   `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Subject              string          `protobuf:"bytes,1,opt,name=subject,proto3" json:"subject,omitempty"`
+	Message              string          `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
+	MessagesForAnswer    map[int64]int32 `protobuf:"bytes,3,rep,name=messagesForAnswer,proto3" json:"messagesForAnswer,omitempty" protobuf_key:"varint,1,opt,name=key,proto3" protobuf_val:"varint,2,opt,name=value,proto3"`
+	XXX_NoUnkeyedLiteral struct{}        `json:"-"`
+	XXX_unrecognized     []byte          `json:"-"`
+	XXX_sizecache        int32           `json:"-"`
 }
 
 func (m *Request) Reset()         { *m = Request{} }
@@ -81,10 +82,18 @@ func (m *Request) GetMessage() string {
 	return ""
 }
 
+func (m *Request) GetMessagesForAnswer() map[int64]int32 {
+	if m != nil {
+		return m.MessagesForAnswer
+	}
+	return nil
+}
+
 type Response struct {
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	MessagesForAnswer    map[int64]int32 `protobuf:"bytes,1,rep,name=messagesForAnswer,proto3" json:"messagesForAnswer,omitempty" protobuf_key:"varint,1,opt,name=key,proto3" protobuf_val:"varint,2,opt,name=value,proto3"`
+	XXX_NoUnkeyedLiteral struct{}        `json:"-"`
+	XXX_unrecognized     []byte          `json:"-"`
+	XXX_sizecache        int32           `json:"-"`
 }
 
 func (m *Response) Reset()         { *m = Response{} }
@@ -120,26 +129,41 @@ func (m *Response) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Response proto.InternalMessageInfo
 
+func (m *Response) GetMessagesForAnswer() map[int64]int32 {
+	if m != nil {
+		return m.MessagesForAnswer
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Request)(nil), "paymentbot.Request")
+	proto.RegisterMapType((map[int64]int32)(nil), "paymentbot.Request.MessagesForAnswerEntry")
 	proto.RegisterType((*Response)(nil), "paymentbot.Response")
+	proto.RegisterMapType((map[int64]int32)(nil), "paymentbot.Response.MessagesForAnswerEntry")
 }
 
 func init() { proto.RegisterFile("paymentbot/paymentbot.proto", fileDescriptor_8ef1d05b6efeaf1a) }
 
 var fileDescriptor_8ef1d05b6efeaf1a = []byte{
-	// 165 bytes of a gzipped FileDescriptorProto
+	// 257 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2e, 0x48, 0xac, 0xcc,
 	0x4d, 0xcd, 0x2b, 0x49, 0xca, 0x2f, 0xd1, 0x47, 0x30, 0xf5, 0x0a, 0x8a, 0xf2, 0x4b, 0xf2, 0x85,
-	0xb8, 0x10, 0x22, 0x4a, 0xb6, 0x5c, 0xec, 0x41, 0xa9, 0x85, 0xa5, 0xa9, 0xc5, 0x25, 0x42, 0x12,
-	0x5c, 0xec, 0xc5, 0xa5, 0x49, 0x59, 0xa9, 0xc9, 0x25, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41,
-	0x30, 0x2e, 0x48, 0x26, 0x37, 0xb5, 0xb8, 0x38, 0x31, 0x3d, 0x55, 0x82, 0x09, 0x22, 0x03, 0xe5,
-	0x2a, 0x71, 0x71, 0x71, 0x04, 0xa5, 0x16, 0x17, 0xe4, 0xe7, 0x15, 0xa7, 0x1a, 0xb9, 0x71, 0x71,
-	0x05, 0x40, 0x0c, 0x76, 0xca, 0x2f, 0x11, 0xb2, 0xe0, 0xe2, 0x0e, 0x4e, 0xcd, 0x4b, 0xf1, 0x85,
-	0x28, 0x14, 0x12, 0xd6, 0x43, 0x72, 0x06, 0xd4, 0x46, 0x29, 0x11, 0x54, 0x41, 0x88, 0x39, 0x4e,
-	0x02, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x8c, 0xc7,
-	0x72, 0x0c, 0x49, 0x6c, 0x60, 0x77, 0x1b, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x84, 0x9c, 0xd8,
-	0xc4, 0xd6, 0x00, 0x00, 0x00,
+	0xb8, 0x10, 0x22, 0x4a, 0x77, 0x19, 0xb9, 0xd8, 0x83, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x84,
+	0x24, 0xb8, 0xd8, 0x8b, 0x4b, 0x93, 0xb2, 0x52, 0x93, 0x4b, 0x24, 0x18, 0x15, 0x18, 0x35, 0x38,
+	0x83, 0x60, 0x5c, 0x90, 0x4c, 0x6e, 0x6a, 0x71, 0x71, 0x62, 0x7a, 0xaa, 0x04, 0x13, 0x44, 0x06,
+	0xca, 0x15, 0x8a, 0xe0, 0x12, 0x84, 0x32, 0x8b, 0xdd, 0xf2, 0x8b, 0x1c, 0xf3, 0x8a, 0xcb, 0x53,
+	0x8b, 0x24, 0x98, 0x15, 0x98, 0x35, 0xb8, 0x8d, 0xb4, 0xf4, 0x90, 0x6c, 0x86, 0xda, 0xa1, 0xe7,
+	0x8b, 0xae, 0xd8, 0x35, 0xaf, 0xa4, 0xa8, 0x32, 0x08, 0xd3, 0x10, 0x29, 0x17, 0x2e, 0x31, 0xec,
+	0x8a, 0x85, 0x04, 0xb8, 0x98, 0xb3, 0x53, 0x2b, 0xc1, 0x6e, 0x64, 0x0e, 0x02, 0x31, 0x85, 0x44,
+	0xb8, 0x58, 0xcb, 0x12, 0x73, 0x4a, 0x21, 0xae, 0x63, 0x0d, 0x82, 0x70, 0xac, 0x98, 0x2c, 0x18,
+	0x95, 0x56, 0x33, 0x72, 0x71, 0x04, 0xa5, 0x16, 0x17, 0xe4, 0xe7, 0x15, 0xa7, 0x0a, 0x45, 0x62,
+	0x73, 0x2c, 0x23, 0xd8, 0xb1, 0xda, 0xa8, 0x8e, 0x85, 0x68, 0xa0, 0xb7, 0x6b, 0x8d, 0xdc, 0xb8,
+	0xb8, 0x02, 0x20, 0xce, 0x70, 0xca, 0x2f, 0x11, 0xb2, 0xe0, 0xe2, 0x0e, 0x4e, 0xcd, 0x4b, 0x81,
+	0x9a, 0x2b, 0x24, 0x8c, 0x25, 0x3c, 0xa5, 0x44, 0xb0, 0xb9, 0xdb, 0x49, 0xe0, 0xc4, 0x23, 0x39,
+	0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf1, 0x58, 0x8e, 0x21, 0x89, 0x0d,
+	0x1c, 0xf5, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x0a, 0xc5, 0xb9, 0xd3, 0x19, 0x02, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -246,6 +270,21 @@ func (m *Request) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.XXX_unrecognized)
 		copy(dAtA[i:], m.XXX_unrecognized)
 	}
+	if len(m.MessagesForAnswer) > 0 {
+		for k := range m.MessagesForAnswer {
+			v := m.MessagesForAnswer[k]
+			baseI := i
+			i = encodeVarintPaymentbot(dAtA, i, uint64(v))
+			i--
+			dAtA[i] = 0x10
+			i = encodeVarintPaymentbot(dAtA, i, uint64(k))
+			i--
+			dAtA[i] = 0x8
+			i = encodeVarintPaymentbot(dAtA, i, uint64(baseI-i))
+			i--
+			dAtA[i] = 0x1a
+		}
+	}
 	if len(m.Message) > 0 {
 		i -= len(m.Message)
 		copy(dAtA[i:], m.Message)
@@ -287,6 +326,21 @@ func (m *Response) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.XXX_unrecognized)
 		copy(dAtA[i:], m.XXX_unrecognized)
 	}
+	if len(m.MessagesForAnswer) > 0 {
+		for k := range m.MessagesForAnswer {
+			v := m.MessagesForAnswer[k]
+			baseI := i
+			i = encodeVarintPaymentbot(dAtA, i, uint64(v))
+			i--
+			dAtA[i] = 0x10
+			i = encodeVarintPaymentbot(dAtA, i, uint64(k))
+			i--
+			dAtA[i] = 0x8
+			i = encodeVarintPaymentbot(dAtA, i, uint64(baseI-i))
+			i--
+			dAtA[i] = 0xa
+		}
+	}
 	return len(dAtA) - i, nil
 }
 
@@ -315,6 +369,14 @@ func (m *Request) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovPaymentbot(uint64(l))
 	}
+	if len(m.MessagesForAnswer) > 0 {
+		for k, v := range m.MessagesForAnswer {
+			_ = k
+			_ = v
+			mapEntrySize := 1 + sovPaymentbot(uint64(k)) + 1 + sovPaymentbot(uint64(v))
+			n += mapEntrySize + 1 + sovPaymentbot(uint64(mapEntrySize))
+		}
+	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
 	}
@@ -327,6 +389,14 @@ func (m *Response) Size() (n int) {
 	}
 	var l int
 	_ = l
+	if len(m.MessagesForAnswer) > 0 {
+		for k, v := range m.MessagesForAnswer {
+			_ = k
+			_ = v
+			mapEntrySize := 1 + sovPaymentbot(uint64(k)) + 1 + sovPaymentbot(uint64(v))
+			n += mapEntrySize + 1 + sovPaymentbot(uint64(mapEntrySize))
+		}
+	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
 	}
@@ -432,6 +502,105 @@ func (m *Request) Unmarshal(dAtA []byte) error {
 			}
 			m.Message = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field MessagesForAnswer", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPaymentbot
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthPaymentbot
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthPaymentbot
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.MessagesForAnswer == nil {
+				m.MessagesForAnswer = make(map[int64]int32)
+			}
+			var mapkey int64
+			var mapvalue int32
+			for iNdEx < postIndex {
+				entryPreIndex := iNdEx
+				var wire uint64
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowPaymentbot
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					wire |= uint64(b&0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				fieldNum := int32(wire >> 3)
+				if fieldNum == 1 {
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowPaymentbot
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						mapkey |= int64(b&0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+				} else if fieldNum == 2 {
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowPaymentbot
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						mapvalue |= int32(b&0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+				} else {
+					iNdEx = entryPreIndex
+					skippy, err := skipPaymentbot(dAtA[iNdEx:])
+					if err != nil {
+						return err
+					}
+					if (skippy < 0) || (iNdEx+skippy) < 0 {
+						return ErrInvalidLengthPaymentbot
+					}
+					if (iNdEx + skippy) > postIndex {
+						return io.ErrUnexpectedEOF
+					}
+					iNdEx += skippy
+				}
+			}
+			m.MessagesForAnswer[mapkey] = mapvalue
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPaymentbot(dAtA[iNdEx:])
@@ -483,6 +652,105 @@ func (m *Response) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Response: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field MessagesForAnswer", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPaymentbot
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthPaymentbot
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthPaymentbot
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.MessagesForAnswer == nil {
+				m.MessagesForAnswer = make(map[int64]int32)
+			}
+			var mapkey int64
+			var mapvalue int32
+			for iNdEx < postIndex {
+				entryPreIndex := iNdEx
+				var wire uint64
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowPaymentbot
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					wire |= uint64(b&0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				fieldNum := int32(wire >> 3)
+				if fieldNum == 1 {
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowPaymentbot
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						mapkey |= int64(b&0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+				} else if fieldNum == 2 {
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowPaymentbot
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						mapvalue |= int32(b&0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+				} else {
+					iNdEx = entryPreIndex
+					skippy, err := skipPaymentbot(dAtA[iNdEx:])
+					if err != nil {
+						return err
+					}
+					if (skippy < 0) || (iNdEx+skippy) < 0 {
+						return ErrInvalidLengthPaymentbot
+					}
+					if (iNdEx + skippy) > postIndex {
+						return io.ErrUnexpectedEOF
+					}
+					iNdEx += skippy
+				}
+			}
+			m.MessagesForAnswer[mapkey] = mapvalue
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPaymentbot(dAtA[iNdEx:])
